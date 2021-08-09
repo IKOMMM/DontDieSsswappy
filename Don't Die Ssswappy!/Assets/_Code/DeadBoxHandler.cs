0011e3a34c9d1d39badb8911a31359b871a78360
IKOMMM <mikolaj.musialik.media@gmail.com>
2021-08-09 22:34:20 +0200

    Added DeadBox Spawner

--- Don't Die Ssswappy!/Assets/_Code/DeadBoxHandler.cs
+++ Don't Die Ssswappy!/Assets/_Code/DeadBoxHandler.cs
@@ -4,13 +4,31 @@ public class DeadBoxHandler : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if(other)
+        if (other)
         {
             PlayerHealthHandler playerHealth = other.GetComponent<PlayerHealthHandler>();
 
-            if(playerHealth == null) { return; }
+            if (playerHealth == null) { return; }
 
             playerHealth.Crash();
         }
     }
+
+    private void Update()
+    {
+        Invoke(nameof(DestroyAfterMoment),10f);
+    }
+
+    void DestroyAfterMoment()
+    {
+        Destroy(gameObject);
+    }
+
+    //Other Solution
+    /*           
+    void OnBecomeInvisible()
+    {
+        Destroy(gameObject);
+    }
+    */
 }
---
+++ Don't Die Ssswappy!/Assets/_Code/DeadBoxSpawner.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadBoxSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] deadBoxPrefabs;
+    [SerializeField] private float secondsBerweenDeadboxes = 1.5f;
+    [SerializeField] private Vector2 forceRange;
+
+    private Camera mainCamera;
+
+    private float timer;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer <= 0)
+        {
+            SpawnDeadBox();
+
+            timer += secondsBerweenDeadboxes;
+        }
+    }
+
+    private void SpawnDeadBox()
+    {
+        int side = Random.Range(0, 4);
+
+        Vector2 spawnPoint = Vector2.zero;
+        Vector2 direction = Vector2.zero;
+
+        switch (side)
+        {
+            case 0: //Left Side
+                spawnPoint.x = 0;
+                spawnPoint.y = Random.value;
+                direction = new Vector2(1f, Random.Range(-1f, 1f));
+                break;
+            case 1: //Right Side
+                spawnPoint.x = 1;
+                spawnPoint.y = Random.value;
+                direction = new Vector2(-1f, Random.Range(-1f, 1f));
+                break;
+            case 2: //Down Side
+                spawnPoint.x = Random.value;
+                spawnPoint.y = 0;
+                direction = new Vector2(Random.Range(-1f, 1f), 1f);
+                break;
+            case 3: //Top Side
+                spawnPoint.x = Random.value;
+                spawnPoint.y = 1;
+                direction = new Vector2(Random.Range(-1f, 1f), -1f);
+                break;
+        }
+
+        Vector3 worldSpawnPoint = mainCamera.ViewportToWorldPoint(spawnPoint);
+        worldSpawnPoint.z = 0;
+
+        GameObject selectedDeadBox = deadBoxPrefabs[Random.Range(0, deadBoxPrefabs.Length)];
+        GameObject deadBoxInstance = Instantiate(
+            selectedDeadBox, 
+            worldSpawnPoint, 
+            Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)));
+
+        Rigidbody rb = deadBoxInstance.GetComponent<Rigidbody>();
+
+        rb.velocity = direction.normalized * Random.Range(forceRange.x, forceRange.y);
+    }
+}
