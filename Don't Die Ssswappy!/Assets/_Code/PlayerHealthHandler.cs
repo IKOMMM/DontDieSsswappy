17feefec72db14c02487dd5136889d78a0f24f5f
IKOMMM <mikolaj.musialik.media@gmail.com>
2021-08-19 15:43:02 +0200

    Added Dead Sound Effect and Dead Particles

--- Don't Die Ssswappy!/Assets/_Code/PlayerHealthHandler.cs
+++ Don't Die Ssswappy!/Assets/_Code/PlayerHealthHandler.cs
@@ -3,11 +3,27 @@ using UnityEngine;
 public class PlayerHealthHandler : MonoBehaviour
 {
     [SerializeField] private GameOverHandler gameOverHandler;
+    [SerializeField] AudioSource deadSound;
+    [SerializeField] ParticleSystem deadParticles;
+    [SerializeField] GameObject ssswappyObj;
+
+
 
     public void Crash()
     {
         gameOverHandler.EndGame();
 
+        ssswappyObj.SetActive(false);
+        deadSound.Play();
+        deadParticles.Play();
+
+        Invoke(nameof(TurnOffPlayer), 0.25f);             
+    }    
+
+    void TurnOffPlayer()
+    {
+        ssswappyObj.SetActive(true);
         gameObject.SetActive(false);
+
     }
 }
